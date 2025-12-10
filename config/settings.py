3b317e6921b11b8b8341a0cf7d03c5ec38198167
zakharfsk <zakhar.fesik@gmail.com>
2025-12-11 01:48:41 +0200

    Add `dj-database-url` and `psycopg2` dependencies; update database configuration
    - Include `dj-database-url` and `psycopg2` in `pyproject.toml`, `requirements.txt`, and lockfiles
    - Refactor `settings.py` to use `DATABASE_URL` environment variable with `dj-database-url`

--- config/settings.py
+++ config/settings.py
@@ -84,10 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.dj_db_url(
+        env.str("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
